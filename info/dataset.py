6897e5e0b99461eef269fcefae12e0ba9eec3e74
arf <goobysc@gmail.com>
2021-02-10 18:54:01 -0500

    added other members to Dataset

--- info/dataset.py
+++ info/dataset.py
@@ -5,5 +5,10 @@ class Dataset:
     def __init__(self, data_members: dict):
         try:
             self.att = ATT(data_members['ATT'])
+            self.coa = COA(data_members['COA'])
+            self.hr = HR(data_members['HR'])
+            self.mysoft = MYSOFT(data_members['MYSOFT'])
+            self.sla = SLA(data_members['SLA'])
+            self.voip = VOIP(data_members['VOIP'])
         except KeyError as e:
             raise Exception(f'[Dataset] missing data member {e.args[0]}')
