abe98281b62b8a7164b99af5ae70e8c494c4cb10
arf <goobysc@gmail.com>
2021-02-10 11:37:23 -0500

    started implementing line datatype

---
+++ datatypes/line.py
@@ -0,0 +1,17 @@
+import re
+
+
+class Line:
+    def __init__(self, directory_number: str, dataset: dict):
+        self._categories: list[str] = [
+            "Phone Number", "User ID", "Name", "Department", "Dept. Code", "Financial Manager", "Building", "Floor",
+            "Room", "Business Set?", "Forward All", "Line Appearances", "Busy Lamp Fields", "Call Pickup Group",
+            "Customer Notes", "UC Notes", "GL String", "Device Model", "Phone Name", "line_type"
+        ]
+
+        self.info: dict = {
+            k: '' for k in self._categories
+        }
+        self.info['Phone Number'] = directory_number
+        self.dn = directory_number
+
