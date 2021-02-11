9b162933a2b2405d66227ae189181ea9a62fab8b
arf <goobysc@gmail.com>
2021-02-11 15:25:30 -0500

    combined all cxm-type classes into one cxm.py file

--- info/formats/__init__.py
+++ info/formats/__init__.py
@@ -1,5 +1,6 @@
 from .att import ATT, ATTEntry
 from .coa import COA, COAEntry
+from .cxm import BLF, BSET, CFD, CPG, LA
 from .hr import HR, HREntry
 from .mysoft import MYSOFT, MYSOFTEntry
 from .sla import SLA, SLAEntry
