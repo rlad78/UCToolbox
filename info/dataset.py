576ccfe66c73739a8c0955c6922b02db886eccc1
arf <goobysc@gmail.com>
2021-02-11 09:38:15 -0500

    added voip and dept info gathering

--- info/dataset.py
+++ info/dataset.py
@@ -16,10 +16,6 @@ class Dataset:
     def get_line_info(self, phone_number: str) -> dict:
         line_info: dict = {
             'Phone Number': phone_number,
-            'bldg_id': '',
-            'Floor': '',
-            'Room': '',
-            'sla_nbr': ''
         }
         att_info: ATTEntry = self.att.get_line(phone_number)
         if att_info is None:
@@ -30,3 +26,35 @@ class Dataset:
             line_info['Room'] = att_info.room
             line_info['sla_nbr'] = att_info.sla
             return line_info
+
+    def get_voip_info(self, phone_number: str) -> dict:
+        voip_info: VOIPEntry = self.voip.get_phone(phone_number)
+        if voip_info is None:
+            return {}
+        else:
+            return {
+                'User ID': voip_info.user_id,
+                'Name': voip_info.description,
+                'Device Mode': '',  # TODO: get method for finding device model
+                'Phone Name': voip_info.device_name
+            }
+
+    def get_dept_info(self, phone_number: str) -> dict:
+        line_info: dict = {}
+        mysoft_info: MYSOFTEntry = self.mysoft.get_line(phone_number)
+        if mysoft_info is None:
+            return {}
+        else:
+            line_info.update({
+                'Dept. Code': mysoft_info.dept,
+                'GL String': mysoft_info.gl
+            })
+            coa_info: COAEntry = self.coa.get_dept(mysoft_info.dept)
+            if coa_info is None:
+                return line_info
+            else:
+                line_info.update({
+                    'Department': coa_info.department,
+                    'Financial Manager': coa_info.financial_manager
+                })
+                return line_info
