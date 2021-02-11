62700819ab23d969a417c21ad631555f81d93e5c
arf <goobysc@gmail.com>
2021-02-10 20:57:04 -0500

    adding functionality to dataset.py

--- info/dataset.py
+++ info/dataset.py
@@ -12,3 +12,21 @@ class Dataset:
             self.voip = VOIP(data_members['VOIP'])
         except KeyError as e:
             raise Exception(f'[Dataset] missing data member {e.args[0]}')
+
+    def get_line_info(self, phone_number: str) -> dict:
+        line_info: dict = {
+            'Phone Number': phone_number,
+            'bldg_id': '',
+            'Floor': '',
+            'Room': '',
+            'sla_nbr': ''
+        }
+        att_info: ATTEntry = self.att.get_line(phone_number)
+        if att_info is None:
+            return line_info
+        else:
+            line_info['bldg_id'] = att_info.bldg_code
+            line_info['Floor'] = att_info.floor
+            line_info['Room'] = att_info.room
+            line_info['sla_nbr'] = att_info.sla
+            return line_info
