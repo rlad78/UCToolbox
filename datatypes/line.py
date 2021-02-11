12b76ac0a300ac2359b93b3ba656a2ffd0cfd57b
arf <goobysc@gmail.com>
2021-02-10 19:01:15 -0500

    was missing "bldg_id" and "sla_nbr" default categories

--- datatypes/line.py
+++ datatypes/line.py
@@ -6,7 +6,7 @@ class Line:
         self._categories: list[str] = [
             "Phone Number", "User ID", "Name", "Department", "Dept. Code", "Financial Manager", "Building", "Floor",
             "Room", "Business Set?", "Forward All", "Line Appearances", "Busy Lamp Fields", "Call Pickup Group",
-            "Customer Notes", "UC Notes", "GL String", "Device Model", "Phone Name", "line_type"
+            "Customer Notes", "UC Notes", "GL String", "Device Model", "Phone Name", "line_type", "bldg_id", "sla_nbr"
         ]
 
         self.info: dict = {
