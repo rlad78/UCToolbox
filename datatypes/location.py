6b224222375e55be7ee80ab170e1c121a5f5443f
arf <goobysc@gmail.com>
2021-02-16 11:50:01 -0500

    sectioned off EMS and EMERGENCY lists

--- datatypes/location.py
+++ datatypes/location.py
@@ -3,6 +3,7 @@ from .sourcedata import Entry
 from fileops import dicts_to_excel
 from pathlib import Path
 from pathvalidate import sanitize_filename, sanitize_filepath
+import re
 
 
 class Location(Entry):
@@ -32,16 +33,12 @@ class Location(Entry):
 
     def pull_lines(self, lines=None) -> list[dict]:
         if lines is None:
-            # return [{k: v} for n in self.lines for k, v in n.info.items()]
-            return []
-        elif type(lines) == list and lines and type(lines[0]) == Line:
-            # return [{k: v} for n in lines for k, v in n.info.items()]
-            line_list: list[dict] = []
-            for line in lines:
-                line_list.append(line.info)
-            return line_list
-        else:
-            return []
+            lines = self.lines
+        # return [{k: v} for n in lines for k, v in n.info.items()]
+        line_list: list[dict] = []
+        for line in lines:
+            line_list.append(line.info)
+        return line_list
 
     def write_centrex_lines(self, root_folder=''):
         if not root_folder:
@@ -51,24 +48,44 @@ class Location(Entry):
             if not path.is_dir():
                 raise Exception(f'[Location.write_lines()]: {root_folder} is not a directory')
 
+        # TODO: do the following sorting WITHIN info (make new Line labels)
         fiman_groups: dict[str, list[Line]] = dict()
         fiman_groups['UNASSIGNED'] = []
+        fiman_groups['EMS'] = []
+        s_ems = re.compile(r'(elev|elv|fire|facp)', re.IGNORECASE)
+
+        fiman_empty = len(fiman_groups)
         for line in self.lines:
             if line['line_type'] == 'VOIP':
                 continue
+
             fiman = line["Financial Manager"]
-            if not fiman:
+            if re.search(s_ems, line['Name']) or re.search(s_ems, line['Room']):
+                fiman_groups['EMS'].append(line)
+            elif not fiman:
                 fiman_groups['UNASSIGNED'].append(line)
             elif fiman in fiman_groups.keys():
                 fiman_groups[fiman].append(line)
             else:
                 fiman_groups[fiman] = [line]
-        if len(fiman_groups) == 1:  # leave if there's no centrex lines
-            return None
 
-        folder = path / sanitize_filepath(f"({len(self.lines)}) {self.building} [SLA {self.sla}]")
-        folder.mkdir(parents=True, exist_ok=True)
-        for fiman, lines in fiman_groups.items():
-            centrex_sum: int = len([ln for ln in lines if ln['line_type'] != "VOIP"])
-            filename = sanitize_filename(f'({centrex_sum}) {self.building} - {fiman}.xlsx')
-            dicts_to_excel(folder / filename, self.pull_lines(lines))
+        # leave if there's no added lines
+        if len(fiman_groups) == fiman_empty and len([ln for ln_list in fiman_groups.values() for ln in ln_list]) == 0:
+            return None
+        # remove empty groups
+        fiman_groups = {k: ln_list for k, ln_list in fiman_groups.items() if ln_list}
+
+        if re.search(r'(EM\s*PH|EP\s|EP\w{3,4}|EMER.*PHONE)', self.building):
+            folder = path / 'EMERGENCY PHONES'
+            if not folder.is_dir():
+                folder.mkdir(parents=True, exist_ok=True)
+            dicts_to_excel(folder / sanitize_filename(f'({len(self.lines)}) {self.building}'), self.pull_lines())
+        else:
+            folder = path / sanitize_filepath(f"({len(self.lines)}) {self.building} [SLA {self.sla}]")
+            folder.mkdir(parents=True, exist_ok=True)
+            for fiman, lines in fiman_groups.items():
+                centrex_sum: int = len([ln for ln in lines if ln['line_type'] != "VOIP"])
+                filename = sanitize_filename(f'({centrex_sum}) {self.building} - {fiman}.xlsx')
+                dicts_to_excel(folder / filename, self.pull_lines(lines))
+            # write out a big sheet for all of them
+            dicts_to_excel(folder / sanitize_filename(f'({len(self.lines)}) {self.building} [ALL]'), self.pull_lines())
