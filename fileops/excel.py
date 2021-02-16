4e95e41dcc2057085df5042b2a2b59073d60aa66
arf <goobysc@gmail.com>
2021-02-16 11:49:23 -0500

    switched to an openpyxl implementation + added text wrap on CMX fields

--- fileops/excel.py
+++ fileops/excel.py
@@ -1,20 +1,33 @@
 import pandas as pd
-import xlsxwriter as xlw
+from openpyxl import styles
+from openpyxl.utils import get_column_letter
 
 
 def dicts_to_excel(filename: str, data: list[dict], sheet_name='Sheet1') -> None:
-    writer = pd.ExcelWriter(filename, engine='xlsxwriter')
+    if str(filename)[-5:] != '.xlsx':
+        filepath = str(filename) + '.xlsx'
+    else:
+        filepath = str(filename)
+    writer = pd.ExcelWriter(filepath, engine='openpyxl')
     df = pd.DataFrame(data)
     df.to_excel(writer, sheet_name=sheet_name)
 
-    worksheet = writer.sheets['Sheet1']
-    for i, width in enumerate(__get_col_widths(df)):
-        worksheet.set_column(i, i, width)
+    worksheet = writer.book.active
 
+    # set col width to match text, and apply text wrap to those who need it
+    for i, col_width in enumerate(__get_col_widths(df)):
+        if worksheet[f'{get_column_letter(i + 1)}1'].value in ['Line Appearances', 'Busy Lamp Fields', 'Call Pickup Group']:
+            worksheet.column_dimensions[get_column_letter(i + 1)].width = len(worksheet[f'{get_column_letter(i + 1)}1'].value) + 1
+            for row in range(2, len(data) + 1, 1):
+                worksheet.cell(row=row,column=i+1).alignment = styles.Alignment(wrap_text=True)
+        else:
+            worksheet.column_dimensions[get_column_letter(i+1)].width = col_width + 1
+
+    # hide unneeded cols
     cols_to_hide = 7
     total_cols = len(data[0].keys()) + 1
-    for n in range(total_cols - cols_to_hide, total_cols):
-        worksheet.set_column(n, n, None, None, {'hidden': 1})
+    for n in range((total_cols - cols_to_hide) + 1, total_cols + 1):
+        worksheet.column_dimensions[get_column_letter(n)].hidden = True
 
     writer.save()
 
