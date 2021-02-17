c97acc607eba2605652326e70eae0857eafd564d
arf <goobysc@gmail.com>
2021-02-17 16:10:21 -0500

    extracted parts from write_centrex_lines and made them into separate methods that don't do any excel writing

--- datatypes/location.py
+++ datatypes/location.py
@@ -40,59 +40,93 @@ class Location(Entry):
             line_list.append(line.info)
         return line_list
 
-    def write_centrex_lines(self, root_folder=''):
-        if not root_folder:
-            path = Path().cwd()
-        else:
-            path = Path(root_folder)
-            if not path.is_dir():
-                raise Exception(f'[Location.write_lines()]: {root_folder} is not a directory')
-
-        # TODO: do the following sorting WITHIN info (make new Line labels)
+    def pull_lines_by_fiman(self) -> dict[str, list[dict]]:
         fiman_groups: dict[str, list[Line]] = dict()
-        fiman_groups['UNASSIGNED'] = []
-        fiman_groups['EMS'] = []
-        s_ems = re.compile(r'(elev|elv|fire|facp)', re.IGNORECASE)
 
-        fiman_empty = len(fiman_groups)
+        # make a separate fiman group for blank/unassigned lines
+        fiman_groups['UNASSIGNED'] = []
         for line in self.lines:
+            fiman = line['Financial Manager']
             if line['line_type'] == 'VOIP':
                 continue
-
-            fiman = line["Financial Manager"]
-            if re.search(s_ems, line['Name']) or re.search(s_ems, line['Room']):
-                fiman_groups['EMS'].append(line)
             elif not fiman:
                 fiman_groups['UNASSIGNED'].append(line)
-            elif fiman in fiman_groups.keys():
-                fiman_groups[fiman].append(line)
+            elif fiman not in fiman_groups.keys():
+                fiman_groups[fiman] = [line]  # if the fiman group hasn't been made yet, make it with a new list
             else:
-                fiman_groups[fiman] = [line]
+                fiman_groups[fiman].append(line)
 
-        # leave if there's no added lines
-        if len(fiman_groups) == fiman_empty and len([ln for ln_list in fiman_groups.values() for ln in ln_list]) == 0:
-            return None
-        # remove empty groups
-        fiman_groups = {k: ln_list for k, ln_list in fiman_groups.items() if ln_list}
+        # # remove blanks and convert Line objects to dict lists
+        return {fi: self.pull_lines(lns) for fi, lns in fiman_groups.items() if lns}
 
+    def pull_ems_lines(self) -> list[dict]:
+        s_ems = re.compile(r'(elev|elv|fire|facp)', re.IGNORECASE)
+
+        # get all lines that have ems keywords in their names or rooms
+        ems_lines: list[Line] = [line for line in self.lines if
+                                 re.search(s_ems, line['Name']) or re.search(s_ems, line['Room'])]
+
+        return self.pull_lines(ems_lines) if ems_lines else []
+
+    def is_emergency_location(self) -> bool:
         if re.search(r'(EM\s*PH|EP\s|EP\w{3,4}|EMER.*PHONE)', self.building):
-            folder = path / 'EMERGENCY PHONES'
-            if not folder.is_dir():
-                folder.mkdir(parents=True, exist_ok=True)
-            dicts_to_excel(folder / sanitize_filename(f'({len(self.lines)}) {self.building}'), self.pull_lines())
-            return {"EMPH": self.pull_lines()}
+            return True
         else:
-            folder = path / sanitize_filepath(f"({len(self.lines)}) {self.building} [SLA {self.sla}]")
-            folder.mkdir(parents=True, exist_ok=True)
-            for fiman, lines in fiman_groups.items():
-                centrex_sum: int = len([ln for ln in lines if ln['line_type'] != "VOIP"])
-                filename = sanitize_filename(f'({centrex_sum}) {self.building} - {fiman}.xlsx')
-                dicts_to_excel(folder / filename, self.pull_lines(lines))
-            # write out a big sheet for all of them (if there's more than 1 sheet)
-            if len(fiman_groups) > 1:
-                dicts_to_excel(folder / sanitize_filename(f'({len(self.lines)}) {self.building} [ALL]'), self.pull_lines())
+            return False
 
-        return fiman_groups
+    # def write_centrex_lines(self, root_folder=''):
+    #     if not root_folder:
+    #         path = Path().cwd()
+    #     else:
+    #         path = Path(root_folder)
+    #         if not path.is_dir():
+    #             raise Exception(f'[Location.write_lines()]: {root_folder} is not a directory')
+    #
+    #     fiman_groups: dict[str, list[Line]] = dict()
+    #     fiman_groups['UNASSIGNED'] = []
+    #     fiman_groups['EMS'] = []
+    #     s_ems = re.compile(r'(elev|elv|fire|facp)', re.IGNORECASE)
+    #
+    #     fiman_empty = len(fiman_groups)
+    #     for line in self.lines:
+    #         if line['line_type'] == 'VOIP':
+    #             continue
+    #
+    #         fiman = line["Financial Manager"]
+    #         if re.search(s_ems, line['Name']) or re.search(s_ems, line['Room']):
+    #             fiman_groups['EMS'].append(line)
+    #         elif not fiman:
+    #             fiman_groups['UNASSIGNED'].append(line)
+    #         elif fiman in fiman_groups.keys():
+    #             fiman_groups[fiman].append(line)
+    #         else:
+    #             fiman_groups[fiman] = [line]
+    #
+    #     # leave if there's no added lines
+    #     if len(fiman_groups) == fiman_empty and len([ln for ln_list in fiman_groups.values() for ln in ln_list]) == 0:
+    #         return None
+    #     # remove empty groups
+    #     fiman_groups = {k: ln_list for k, ln_list in fiman_groups.items() if ln_list}
+    #
+    #     if re.search(r'(EM\s*PH|EP\s|EP\w{3,4}|EMER.*PHONE)', self.building):
+    #         folder = path / 'EMERGENCY PHONES'
+    #         if not folder.is_dir():
+    #             folder.mkdir(parents=True, exist_ok=True)
+    #         dicts_to_excel(folder / sanitize_filename(f'({len(self.lines)}) {self.building}'), self.pull_lines())
+    #         return {"EMPH": self.pull_lines()}
+    #     else:
+    #         folder = path / sanitize_filepath(f"({len(self.lines)}) {self.building} [SLA {self.sla}]")
+    #         folder.mkdir(parents=True, exist_ok=True)
+    #         for fiman, lines in fiman_groups.items():
+    #             centrex_sum: int = len([ln for ln in lines if ln['line_type'] != "VOIP"])
+    #             filename = sanitize_filename(f'({centrex_sum}) {self.building} - {fiman}.xlsx')
+    #             dicts_to_excel(folder / filename, self.pull_lines(lines))
+    #         # write out a big sheet for all of them (if there's more than 1 sheet)
+    #         if len(fiman_groups) > 1:
+    #             dicts_to_excel(folder / sanitize_filename(f'({len(self.lines)}) {self.building} [ALL]'),
+    #               self.pull_lines())
+    #
+    #     return fiman_groups
 
     def summary(self) -> dict:
         info: dict = {
