03d56201860d29c597005177c525ac77fd89ce54
arf <goobysc@gmail.com>
2021-02-17 12:02:28 -0500

    created write_fire for exporting fire alarms to xl

--- main.py
+++ main.py
@@ -61,8 +61,22 @@ def write_buildings(dataset=None):
     summary_file = output_folder / "SUMMARY.csv"
     csv_from_dicts(str(summary_file), [d for d in results.values()])
 
+def write_fire(dataset=None):
+    database: Database = db.get_db(dataset)
+    fire_temp: list[dict] = database.parseall("Name", r'(fire|facp)', re.I)
+    fire_temp += (database.parseall("Name", r'(fire|facp)', re.I))
+
+    #remove duplicates and voip lines
+    fire_lines: list[dict] = []
+    fire_dns: list[str] = []
+    for line in fire_temp:
+        if line['Phone Number'] not in fire_dns and line['line_type'] != 'VOIP':
+            fire_lines.append(line)
+            fire_dns.append(line['Phone Number'])
+
+    p = get_output_folder()
+    dicts_to_excel(p / "FIRE ALARMS.xlsx", fire_lines, sheet_name="FIRE")
+
+
 if __name__ == '__main__':
-    # csv_from_dicts('ucdb.csv', generate_db(load_dataset()))
-    # print(search_line_demo(input('Phone number: ')))
-    # preview_buildings(data.load_dataset())
-    write_buildings(data.load_dataset())
+    write_fire(data.load_dataset())
