6e576b8b871292a66fc8d601968f03c04d66862d
arf <goobysc@gmail.com>
2021-03-04 16:23:50 -0500

    created print_columns

--- main.py
+++ main.py
@@ -10,6 +10,12 @@ def search_line_demo(phone_number: str) -> Line:
     return me
 
 
+def print_columns(data: list[dict], *args) -> None:
+    df = pd.DataFrame(data)
+    big = pd.DataFrame({s: df[s] for s in args})
+    print(big)
+
+
 if __name__ == '__main__':
     ucdb = db.get_db()
-    print(pd.DataFrame(ucdb.elevator_lines())[['Phone Number', 'Name', 'Room']])
+    print_columns(ucdb.elevator_lines(), 'Phone Number', 'Name', 'Room')
