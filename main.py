9deba900296344b163fee826a2af22a962081b41
arf <goobysc@gmail.com>
2021-03-09 10:39:22 -0500

    testing

--- main.py
+++ main.py
@@ -6,7 +6,5 @@ import re
 
 if __name__ == '__main__':
     ucdb = db.get_db()
-    upper_lines = ucdb.parseall("Phone Number", r'^86465604\d\d', re.I)
-    less_lines = upper_lines.parse("Phone Number", r'864656041\d')
-    line_types: dict[str, ResultData] = less_lines.group_by('Financial Manager')
-    print(line_types.keys())
+    emg_lines = ucdb.emergency_phones()
+    print(emg_lines)
